39361209ac5f7ffd89adf2f5467cfc7097193424
Gnome404 <Alliraheem3532@gmail.com>
2021-03-16 21:42:58 +0400

    First Commit

---
+++ product.js
@@ -0,0 +1,51 @@
+let products = [{
+        id: 111,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em> irresistible </em>," +
+            "organic goodness for your cat.",
+        price: 2000,
+        image: 'image/cat.png',
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 112,
+        title: "Dog Food, 25lb bag",
+        description: "A 25 pound bag of <em> irresistible </em>," +
+            "organic goodness for your cat.",
+        price: 2000,
+        image: 'image/Dog.png',
+        availableInventory: 10,
+        rating: 5
+    },
+    {
+        id: 113,
+        title: "Fish Food, 25lb bag",
+        description: "A 25 pound bag of <em> irresistible </em>," +
+            "organic goodness for your cat.",
+        price: 2000,
+        image: 'image/logo_ 1.png',
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 114,
+        title: "Mel, 25lb bag",
+        description: "A 25 pound bag of <em> irresistible </em>," +
+            "organic goodness for your cat.",
+        price: 2000,
+        image: 'image/logo_ 1.png',
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id: 115,
+        title: "Emma Food, 25lb bag",
+        description: "A 25 pound bag of <em> irresistible </em>," +
+            "organic goodness for your cat.",
+        price: 2000,
+        image: 'image/logo_ 1.png',
+        availableInventory: 10,
+        rating: 1
+    },
+]
